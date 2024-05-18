eabdbca58fb4833ad88f41cc00082ec78730cff0
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-05-18 23:08:30 +0530

    inital commit

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { app } from './app.js'
+// import connectDB from "./db/index.js";
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 2020, () => {
+//       console.log("app is runnign at the above");
+//     });
+//   })
+//   .catch((er) => {
+//     console.log(er);
+//   });
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'hello there',
+  })
+})
+
+app.listen(2021, () => {
+  console.log('app is runnign at the above')
+})
